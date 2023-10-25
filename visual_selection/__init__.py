d915cb7ce4df21e86af89c26012f378c09708215
Dave Pearson <davep@davep.org>
2023-10-25 08:39:49 +0100

    :rocket: Add packing and shipping help

---
+++ visual_selection/__init__.py
@@ -0,0 +1,17 @@
+"""A tool to illustrate mutation in evolution."""
+
+######################################################################
+# Main app information.
+__author__ = "Dave Pearson"
+__copyright__ = "Copyright 2023, Dave Pearson"
+__credits__ = ["Dave Pearson"]
+__maintainer__ = "Dave Pearson"
+__email__ = "davep@davep.org"
+__version__ = "0.0.1"
+__licence__ = "GPLv3+"
+
+##############################################################################
+# Public symbols.
+__all__ = []
+
+### __init__.py ends here
