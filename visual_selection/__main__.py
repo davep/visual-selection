77c6a28d2edcd857d310804b0e9d6223b7f6b5e9
Dave Pearson <davep@davep.org>
2023-10-25 08:43:09 +0100

    :zap: Fix the main script entry point

--- visual_selection/__main__.py
+++ visual_selection/__main__.py
@@ -2,7 +2,13 @@
 
 from .app import SelectionApp
 
-if __name__ == "__main__":
+
+def run() -> None:
+    """Run the application."""
     SelectionApp().run()
+
+
+if __name__ == "__main__":
+    run()
 
 ### __main__.py ends here
